5594d9f853f78a485c277f950c37e294e6232527
Krish-log <gopi.maganti1998@gmail.com>
2025-02-17 14:43:35 -0500

    Initial Commit

---
+++ src/test/java/Gopi/java/Todo_application/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package Gopi.java.Todo_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
